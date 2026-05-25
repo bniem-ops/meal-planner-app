6c893b7cc5113301f91e06cf5d34bd1209fb47c5
Brent Niemerski <brentcniemerski@gmail.com>
2026-05-25 12:00:37 -0400

    firebase Config

--- meal-planner/src/lib/firebase.js
+++ meal-planner/src/lib/firebase.js
@@ -2,13 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCIIz8I-FtD-gz8N6TmzFzcssbQQAHOSmA",
+  authDomain: "bn-meal-planning.firebaseapp.com",
+  projectId: "bn-meal-planning",
+  storageBucket: "bn-meal-planning.firebasestorage.app",
+  messagingSenderId: "63544590088",
+  appId: "1:63544590088:web:9be810894230ec45cdf6bd",
+  measurementId: "G-N9Q7DPK23J"
 };
 
 const app = initializeApp(firebaseConfig);
